8fdb7bf153976c04e1071b9aeb9391c5134f4e90
wish-mobile <conan@wish-mobile.com>
2023-10-19 16:02:24 +0900

    Auth Button 추가

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/common/widgets/views/app_font.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void onLoginTap(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +35,16 @@ class SignUpScreen extends StatelessWidget {
                 color: Colors.black45,
                 textAlign: TextAlign.center,
               ),
+              Gaps.v40,
+              AuthButton(
+                icon: FaIcon(FontAwesomeIcons.user),
+                text: "Use phone or email",
+              ),
+              Gaps.v16,
+              AuthButton(
+                icon: FaIcon(FontAwesomeIcons.apple),
+                text: "Continue with Apple",
+              ),
             ],
           ),
         ),
@@ -41,12 +59,18 @@ class SignUpScreen extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const AppFont("Already have an account?"),
+              const AppFont(
+                "Already have an account?",
+                size: 16,
+              ),
               Gaps.h5,
-              AppFont(
-                "Log in",
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.w600,
+              GestureDetector(
+                onTap: () => onLoginTap(context),
+                child: AppFont(
+                  "Log in",
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ],
           ),
