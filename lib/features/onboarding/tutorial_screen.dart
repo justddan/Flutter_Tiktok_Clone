439c7c9405ba81a1fb5c77b34ac2de028d8c27a1
wish-mobile <conan@wish-mobile.com>
2023-10-20 22:47:32 +0900

    Tutorial 스크린 퍼블리시 2

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/common/widgets/views/app_font.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -11,8 +16,100 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showinPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails detail) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showinPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showinPage = Page.first;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  AppFont(
+                    "Watch cool videos!",
+                    size: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  Gaps.v16,
+                  AppFont(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    size: Sizes.size20,
+                  )
+                ],
+              ),
+              secondChild: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  AppFont(
+                    "Follow the rules!",
+                    size: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  Gaps.v16,
+                  AppFont(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    size: Sizes.size20,
+                  )
+                ],
+              ),
+              crossFadeState: _showinPage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(milliseconds: 300),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size48, horizontal: Sizes.size24),
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _showinPage == Page.first ? 0 : 1,
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text("Enter the app!"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    /*
     return DefaultTabController(
       length: 3,
       child: Scaffold(
@@ -101,5 +198,6 @@ class _TutorialScreenState extends State<TutorialScreen> {
         ),
       ),
     );
+    */
   }
 }
