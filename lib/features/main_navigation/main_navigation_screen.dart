b19fdc5769f6ad6bb632099b7ce5dbfe60256b27
wish-mobile <conan@wish-mobile.com>
2023-10-21 23:28:49 +0900

    CupertinoTabBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,96 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/common/widgets/views/app_font.dart';
 
-class MainNavigation extends StatelessWidget {
+class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
   @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: AppFont("Home"),
+    ),
+    const Center(
+      child: AppFont("Search"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    // cupertinoapp으로 변경
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(CupertinoIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(CupertinoIcons.search),
+            label: "Search",
+            tooltip: "What are you?",
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) => screens[index],
+    );
+    /*return Scaffold(
+      body: screens[_selectedIndex],
+      /*bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onTap,
+        destinations: const [
+          NavigationDestination(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+              color: Colors.teal,
+            ),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: Colors.amber,
+            ),
+            label: "Search",
+          ),
+        ],
+      ),
+      */
+      /*
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What are you?",
+          ),
+        ],
+      ),
+      */
+    );
+    */
   }
 }
