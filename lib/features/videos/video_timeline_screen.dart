f5b0b92d1774b6169383fe307225fbc848f32501
wish-mobile <conan@wish-mobile.com>
2023-10-23 22:19:30 +0900

    video_player를 통한 Home 스크린 퍼블리시

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/common/widgets/views/app_font.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -9,7 +9,12 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
-  int _itemCount = 4;
+  final int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
   List<Color> colors = [
     Colors.blue,
@@ -18,31 +23,44 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     Colors.green,
   ];
 
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   void _onPageChanged(int page) {
-    if (page == _itemCount - 1) {
-      _itemCount = _itemCount + 4;
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.green,
-      ]);
-      setState(() {});
-    }
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+    // if (page == _itemCount - 1) {
+    //   _itemCount = _itemCount + 4;
+    //   colors.addAll([
+    //     Colors.blue,
+    //     Colors.red,
+    //     Colors.yellow,
+    //     Colors.green,
+    //   ]);
+    //   setState(() {});
+    // }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
+      controller: _pageController,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => Container(
-        color: colors[index],
-        child: Center(
-          child: AppFont(
-            "Screen $index",
-            size: 68,
-          ),
-        ),
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
       ),
       onPageChanged: _onPageChanged,
       scrollDirection: Axis.vertical,
