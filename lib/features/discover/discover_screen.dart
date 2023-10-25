f102f5d3b52f61af997bc0f83e7efe6a4dda2be0
wish-mobile <conan@wish-mobile.com>
2023-10-25 17:47:23 +0900

    Discover 스크린 GridViewItem FadeInImage

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/common/widgets/views/app_font.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -55,14 +57,62 @@ class DiscoverScreen extends StatelessWidget {
                 crossAxisCount: 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 20,
               ),
               itemBuilder: (context, index) {
-                return Container(
-                  color: Colors.teal,
-                  child: Center(
-                    child: AppFont("$index"),
-                  ),
+                return Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/placeholder.jpg",
+                        image:
+                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+                      ),
+                    ),
+                    Gaps.v10,
+                    const AppFont(
+                      "This is a very long caption for my tiktok tha im upload just now currently.",
+                      size: Sizes.size16,
+                      fontWeight: FontWeight.bold,
+                      maxLine: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Gaps.v8,
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/3612017",
+                          ),
+                        ),
+                        Gaps.h4,
+                        Expanded(
+                          child: AppFont(
+                            "My avatar is going to be very long",
+                            maxLine: 1,
+                            overflow: TextOverflow.ellipsis,
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        AppFont(
+                          "2.5M",
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ],
+                    )
+                  ],
                 );
               },
             ),
