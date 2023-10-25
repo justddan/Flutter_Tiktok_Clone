0f29c5d8bd66c0a45002a8518df365eeb1e02b68
wish-mobile <conan@wish-mobile.com>
2023-10-25 19:38:03 +0900

    Inbox 스크린 퍼블리시 ListTile

---
+++ lib/features/inbox/inbox_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/common/widgets/views/app_font.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({super.key});
+
+  void _onDmPressed() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: const Text("Inbox"),
+        actions: [
+          IconButton(
+            onPressed: _onDmPressed,
+            icon: const FaIcon(
+              FontAwesomeIcons.paperPlane,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {},
+            title: const AppFont(
+              "Activity",
+              fontWeight: FontWeight.w600,
+              size: Sizes.size16,
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            height: Sizes.size1,
+            color: Colors.grey.shade200,
+          ),
+          ListTile(
+            leading: Container(
+              width: Sizes.size52,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.users,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: const AppFont(
+              "New followers",
+              fontWeight: FontWeight.w600,
+              size: Sizes.size16,
+            ),
+            subtitle: const Text(
+              "Message from followers will appear here",
+              style: TextStyle(
+                fontSize: Sizes.size14,
+              ),
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/discover/discover_screen.dart';
+import 'package:tiktok_clone/features/inbox/inbox_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
@@ -15,7 +16,7 @@ class MainNavigation extends StatefulWidget {
 }
 
 class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 3;
 
   /*final screens = [
     const Center(
@@ -87,7 +88,7 @@ class _MainNavigationState extends State<MainNavigation> {
           ),
           Offstage(
             offstage: _selectedIndex != 3,
-            child: Container(),
+            child: const InboxScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 4,
