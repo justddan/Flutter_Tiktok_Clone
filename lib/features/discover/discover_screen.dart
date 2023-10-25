3d9be29fafb9a2c3fc74a888092859d443d82a54
wish-mobile <conan@wish-mobile.com>
2023-10-25 17:03:46 +0900

    Discover 스크린 GridView

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -46,7 +46,27 @@ class DiscoverScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            for (var tab in tabs)
+            GridView.builder(
+              itemCount: 20,
+              padding: const EdgeInsets.all(
+                Sizes.size6,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 16,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.teal,
+                  child: Center(
+                    child: AppFont("$index"),
+                  ),
+                );
+              },
+            ),
+            for (var tab in tabs.skip(1))
               Center(
                 child: AppFont(
                   tab,
