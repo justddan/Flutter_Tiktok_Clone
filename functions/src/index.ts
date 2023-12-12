044c14993f98178c36dec778c6356baf6e696fbb
wish-mobile <conan@wish-mobile.com>
2023-12-12 19:21:21 +0900

    Firebase Functions 2 - ffmpeg로 썸네일 만들기

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,10 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
-export const onVideoCreated = functions.firestore.document('videos/{vidoId}').onCreate(async (snapshot, context) => {
-  snapshot.ref.update({ thumbnailUrl: 'from functions' });
+export const onVideoCreated = functions.firestore.document('videos/{videoId}').onCreate(async (snapshot, context) => {
+  const spawn = require('child-process-promise').spawn;
+  const video = snapshot.data();
+  await spawn('ffmpeg', ['-i', video.fileUrl, '-ss', '00:00:01.000', '-vframes', '1', '-vf', 'scale=150:-1', `/tmp/${snapshot.id}.jpg`]);
+  const storage = admin.storage();
+  storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, { destination: `thumbnails/${snapshot.id}.jpg` });
 });
