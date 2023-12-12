bc0dc9c5213a41bb26e74f56c3d4339aeef644de
wish-mobile <conan@wish-mobile.com>
2023-12-12 20:46:20 +0900

    업로드 후 라우팅 작업

--- lib/features/videos/view_model/upload_vidoe_view_model.dart
+++ lib/features/videos/view_model/upload_vidoe_view_model.dart
@@ -39,7 +39,8 @@ class UploadVideoViewModel extends AsyncNotifier<void> {
             comments: 0,
             createdAt: DateTime.now().millisecondsSinceEpoch,
           ));
-          context.pushReplacement("/home");
+          context.pop();
+          context.pop();
         }
       });
     }
